8e0f4e74c51932375d2f04946c3728eea6c4fa16
Niloy Julious <niloyjulious@gmail.com>
2022-02-15 23:02:40 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meals_app
+ App for restaurants to see menu and food recipes
---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/meal_item.dart';
+import 'package:meals_app/models/meals.dart';
+
+class FavoritesScreen extends StatelessWidget {
+
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('You have no favourites yet! Start adding some!'),),
+
+        body: Center(
+          child: Text('Favorites'),
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
